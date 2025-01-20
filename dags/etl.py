ee0cabe7c4c051d9c0b4338b8df28b39a3166d82
ayman-zouini <zouiniayman321@gmail.com>
2025-01-20 21:35:14 +0000

    feat: update DAG with new ETL components

--- dags/etl.py
+++ dags/etl.py
@@ -1,77 +1,10 @@
 from datetime import timedelta, datetime
-
 from airflow.decorators import dag, task
-from airflow.providers.sqlite.hooks.sqlite import SqliteHook
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
-
-TABLES_CREATION_QUERY = """CREATE TABLE IF NOT EXISTS job (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title VARCHAR(225),
-    industry VARCHAR(225),
-    description TEXT,
-    employment_type VARCHAR(125),
-    date_posted DATE
-);
-
-CREATE TABLE IF NOT EXISTS company (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    job_id INTEGER,
-    name VARCHAR(225),
-    link TEXT,
-    FOREIGN KEY (job_id) REFERENCES job(id)
-);
-
-CREATE TABLE IF NOT EXISTS education (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    job_id INTEGER,
-    required_credential VARCHAR(225),
-    FOREIGN KEY (job_id) REFERENCES job(id)
-);
-
-CREATE TABLE IF NOT EXISTS experience (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    job_id INTEGER,
-    months_of_experience INTEGER,
-    seniority_level VARCHAR(25),
-    FOREIGN KEY (job_id) REFERENCES job(id)
-);
-
-CREATE TABLE IF NOT EXISTS salary (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    job_id INTEGER,
-    currency VARCHAR(3),
-    min_value NUMERIC,
-    max_value NUMERIC,
-    unit VARCHAR(12),
-    FOREIGN KEY (job_id) REFERENCES job(id)
-);
-
-CREATE TABLE IF NOT EXISTS location (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    job_id INTEGER,
-    country VARCHAR(60),
-    locality VARCHAR(60),
-    region VARCHAR(60),
-    postal_code VARCHAR(25),
-    street_address VARCHAR(225),
-    latitude NUMERIC,
-    longitude NUMERIC,
-    FOREIGN KEY (job_id) REFERENCES job(id)
-)
-"""
-
-@task()
-def extract():
-    """Extract data from jobs.csv."""
-
-@task()
-def transform():
-    """Clean and convert extracted elements to json."""
-
-@task()
-def load():
-    """Load data to sqlite database."""
-    sqlite_hook = SqliteHook(sqlite_conn_id='sqlite_default')
+from constants import TABLES_CREATION_QUERY
+from extract import JobsExtractor
+from transform import JobsTransformer
+from load import SqliteLoader
 
 DAG_DEFAULT_ARGS = {
     "depends_on_past": False,
@@ -89,14 +22,27 @@ DAG_DEFAULT_ARGS = {
     default_args=DAG_DEFAULT_ARGS
 )
 def etl_dag():
-    """ETL pipeline"""
+    @task()
+    def extract():
+        extractor = JobsExtractor()
+        return extractor.extract()
+        
+    @task()
+    def transform():
+        transformer = JobsTransformer()
+        return transformer.transform()
+        
+    @task()
+    def load():
+        loader = SqliteLoader()
+        return loader.load()
 
     create_tables = SqliteOperator(
         task_id="create_tables",
         sqlite_conn_id="sqlite_default",
         sql=TABLES_CREATION_QUERY
     )
-
+    
     create_tables >> extract() >> transform() >> load()
 
 etl_dag()
