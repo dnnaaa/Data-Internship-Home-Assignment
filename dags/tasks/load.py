8f75403570f442fb44eb8f52a4df0a5509a97f46
younes921722 <younes.saouabeddine2001@gmail.com>
2025-01-21 12:38:21 +0100

    feat: fill dags/tasks/load.py with funcs

--- dags/tasks/load.py
+++ dags/tasks/load.py
@@ -1,11 +1,12 @@
 from airflow.decorators import task
 from airflow.providers.sqlite.hooks.sqlite import SqliteHook
 from utils.logger import get_logger
-from utils.db_loader import load_table_data
+from utils.db_loader import load_jobs_to_database
 
 logger = get_logger(__name__)
 
 @task()
 def load_data():
     """Load transformed data into SQLite database."""
-    
+    load_jobs_to_database()
+    print("Data successfully loaded into normalized SQLite database")
